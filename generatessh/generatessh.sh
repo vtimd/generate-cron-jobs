8b99c80bbaa4eac1cde04837cf17f337e5b636a6
Tim Davis <timd@vmware.com>
2018-08-16 15:12:03 -0500

    Structure changes, file changes

--- generatessh/generatessh.sh
+++ generatessh/generatessh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#Place this file in /etc/cron.hourly
+#Place the serverlist file in a directory called /generatessh on MGMT
 
 LINUX_COMMAND='uptime'
 
-for i in `cat /files/serverlist`
+for i in `cat /generatessh/serverlist`
 do
     ssh ${i} ${LINUX_COMMAND}
 done
